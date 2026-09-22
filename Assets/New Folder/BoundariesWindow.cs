96ba6a087e18765a30b3224d76f5c6b6b87a61d3
robert <1035695@student.sae.edu.au>
2026-09-22 13:09:39 +1000

    Organization of Layout

--- Assets/New Folder/BoundariesWindow.cs
+++ Assets/New Folder/BoundariesWindow.cs
@@ -1,26 +1,62 @@
 using UnityEngine;
 using UnityEditor;
+using System.ComponentModel;
+using UnityEngine.UIElements;
 
 public class BoundariesWindow : EditorWindow
 {
     public float height;
     public float depth;
 
+    public int nodeCount;
+
+    public string tabs;
+
     public Color nodeColour;
     public Color ColliderColour;
 
     public bool isVisible;
     public bool isClosedLoop;
-    
+
+    public string currentPanel;
     [MenuItem("Window/Boundaries")]
     public static void ShowWindow()
     {
         GetWindow<BoundariesWindow>("Boundaries");
     }
 
+    //private void OnEnable()
+    //{
+    //     TabView tabview = new TabView();
+    //    Tab tab1 = new Tab("Nodes");
+    //    tab1.Add(new Label("work with nodes"));
+
+    //    Tab tab2 = new Tab("colour");
+    //    tab2.Add(new Button(() => Debug.Log("Colour tools")) { text = "run colour"});
+
+    //    tabview.Add(tab1);
+    //    tabview.Add(tab2);
+
+    //    rootVisualElement.Add(tabview);
+    //}
     private void OnGUI()
     {
-        GUILayout.Label("Boundaries", EditorStyles.boldLabel);
+        //tabs = GUILayout.Toolbar(tabs, new string[] { "nodes", "colour" });
+        //switch()
+        GUILayout.Label("Nodes", EditorStyles.boldLabel);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Add Node"))
+        {
+            //adds a Node
+        }
+        if (GUILayout.Button("Remove Node"))
+        {
+            //remove selected node OR Last node
+        }
+        GUILayout.EndHorizontal();
+
+
 
         if (GUILayout.Button("Reset"))
         {
@@ -32,10 +68,9 @@ public class BoundariesWindow : EditorWindow
             //Confirms the Boundaries
         }
 
-        if(GUILayout.Button("Add Node"))
-        {
-            //adds a Node
-        }
+
+        nodeCount = EditorGUILayout.IntField("Base Count", nodeCount);
+
         EditorGUILayout.Space();
 
         EditorGUILayout.LabelField("Dimention", EditorStyles.boldLabel);
@@ -48,6 +83,9 @@ public class BoundariesWindow : EditorWindow
         ColliderColour = EditorGUILayout.ColorField("Collider Colour", ColliderColour);
 
         if (GUILayout.Button("Confirm Colour"))
+        {
+            //
+        }
         EditorGUILayout.Space();
 
         EditorGUILayout.LabelField("Toggles", EditorStyles.boldLabel);
@@ -55,12 +93,12 @@ public class BoundariesWindow : EditorWindow
         isVisible = EditorGUILayout.Toggle("Visiblility", isVisible);
         isClosedLoop = EditorGUILayout.Toggle("Closed Loop", isClosedLoop);
 
-
-        
         if (GUILayout.Button("Close"))
         {
+            // closes Window panel
             this.Close();
         }
 
     }
+    
 }
